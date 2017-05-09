461145cf1f611ed4655f514eba71bf126f3363cf
Lucas Sotty <lucas.sotty@wanadoo.fr>
2017-05-09 14:32:48 +0200

    Exercise 1 solved

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+
+  patch "tasks/:id/done", to: "tasks#mark_as_done", as: "mark"
 end
